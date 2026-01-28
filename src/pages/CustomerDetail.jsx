722ee7d3e525a668f0836160b202bb783dcc6540
Ruben Van Der Velden <rubend3315@gmail.com>
2026-01-28 18:39:07 +0000

    File changes

--- src/pages/CustomerDetail.jsx
+++ src/pages/CustomerDetail.jsx
@@ -194,6 +194,10 @@ export default function CustomerDetail() {
             <Upload className="w-4 h-4" />
             Imports ({imports.length})
           </TabsTrigger>
+          <TabsTrigger value="history" className="gap-2">
+            <History className="w-4 h-4" />
+            Geschiedenis
+          </TabsTrigger>
           <TabsTrigger value="calculations" className="gap-2">
             <Calculator className="w-4 h-4" />
             Berekeningen
