e88d2db2fc9958fb58bb218ba9c2faf90c72c66c
Ruben Van Der Velden <rubend3315@gmail.com>
2026-01-28 19:21:34 +0000

    File changes

--- src/components/customer/ImportExcelModal.jsx
+++ src/components/customer/ImportExcelModal.jsx
@@ -93,6 +93,7 @@ export default function ImportExcelModal({ open, onOpenChange, customerId, custo
         ...createdImport,
         validationErrors: validationErrors
       });
+      setShowValidation(false);
     }
   });
 
