b6df224f3d0e5ac7fcb308a59627e34451809ea3
Ruben Van Der Velden <rubend3315@gmail.com>
2026-01-28 17:00:06 +0000

    File changes

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { createPageUrl } from "@/utils";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
