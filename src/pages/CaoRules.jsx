da16cf63d14731843bcf78854c83eea0651ca366
Ruben Van Der Velden <rubend3315@gmail.com>
2026-01-29 10:13:39 +0000

    File changes

--- src/pages/CaoRules.jsx
+++ src/pages/CaoRules.jsx
@@ -194,7 +194,7 @@ export default function CaoRules() {
             Artikel 19 Inschaling bij indiensttreding
           </TabsTrigger>
           <TabsTrigger value="jonge-werknemers" className="justify-start">
-            Artikel 22 21 jarigen en ouder/ vakbekwame chauffeurs
+            Mintrede-niet vakbekwaam
           </TabsTrigger>
         </TabsList>
         
