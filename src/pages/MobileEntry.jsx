31cd57c02d80753d45b612424b0638b9233fe4ae
Ruben Van Der Velden <rubend3315@gmail.com>
2026-01-30 17:22:13 +0000

    File changes

--- src/pages/MobileEntry.jsx
+++ src/pages/MobileEntry.jsx
@@ -390,14 +390,6 @@ export default function MobileEntry() {
     return Math.round(totalMinutes / 60 * 100) / 100;
   };
 
-  const getWeekNumber = (date) => {
-    const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-    const dayNum = d.getUTCDay() || 7;
-    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
-    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-    return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
-  };
-
   const approvedEntries = myTimeEntries.filter(e => e.status === 'Goedgekeurd');
 
   const handleLogout = () => {
