a5cc530f7ddbaac157b3f4de32a5a0757cffc62d
Ruben Van Der Velden <rubend3315@gmail.com>
2026-01-30 18:03:17 +0000

    File changes

--- src/pages/MobileEntry.jsx
+++ src/pages/MobileEntry.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useOfflineSync } from "@/components/utils/useOfflineSync";
+import OfflineSyncIndicator from "@/components/OfflineSyncIndicator";
 import { format, getWeek, getYear } from "date-fns";
 import { nl } from "date-fns/locale";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
