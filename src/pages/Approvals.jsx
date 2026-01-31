5a3738ada0a79450443d09fecca1e9d8af1d7c31
Ruben Van Der Velden <rubend3315@gmail.com>
2026-01-31 21:12:40 +0000

    File changes

--- src/pages/Approvals.jsx
+++ src/pages/Approvals.jsx
@@ -23,7 +23,7 @@ import {
   Eye,
   Edit
 } from "lucide-react";
-import { getBreakScheduleMinutes } from "@/components/utils/breakScheduleUtils";
+import { getBreakMinutesForHours } from "@/components/utils/breakScheduleUtils";
 
 export default function Approvals() {
   const [selectedEntry, setSelectedEntry] = useState(null);
