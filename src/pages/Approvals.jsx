8a72441e51cf9a10c3bc888dacbd7cd7779f896e
Ruben Van Der Velden <rubend3315@gmail.com>
2026-01-31 21:17:00 +0000

    File changes

--- src/pages/Approvals.jsx
+++ src/pages/Approvals.jsx
@@ -12,6 +12,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   CheckCircle,
   XCircle,
