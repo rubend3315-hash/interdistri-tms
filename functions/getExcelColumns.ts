eef84022706e4a3a98107c0480cf0400133eee49
Ruben Van Der Velden <rubend3315@gmail.com>
2026-02-02 20:49:53 +0000

    File changes

--- functions/getExcelColumns.ts
+++ functions/getExcelColumns.ts
@@ -4,16 +4,24 @@ Deno.serve(async (req) => {
   try {
     const base44 = createClientFromRequest(req);
     
-    // Haal alle PostNLImportResult records op
-    const imports = await base44.asServiceRole.entities.PostNLImportResult.list();
-    
     const columnsSet = new Set();
     
-    // Verzamel alle unieke kolommen uit alle data objects
+    // Kolommen uit PostNLImportResult
+    const imports = await base44.asServiceRole.entities.PostNLImportResult.list();
     imports.forEach(imp => {
       if (imp.data && typeof imp.data === 'object') {
         Object.keys(imp.data).forEach(key => columnsSet.add(key));
       }
+    });
+    
+    // Basis velden uit RapportageRit
+    const rapportageRitten = await base44.asServiceRole.entities.RapportageRit.list();
+    rapportageRitten.forEach(rit => {
+      Object.keys(rit).forEach(key => {
+        if (!['data', 'artikelen', 'id', 'created_date', 'updated_date', 'created_by'].includes(key)) {
+          columnsSet.add(key);
+        }
+      });
     });
     
     const columns = Array.from(columnsSet).sort();
