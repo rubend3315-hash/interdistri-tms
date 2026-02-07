af23ff93d0d2227056589ca3abb525ba4c17145e
Ruben Van Der Velden <rubend3315@gmail.com>
2026-02-07 19:10:50 +0000

    File changes

--- src/pages/Vehicles.jsx
+++ src/pages/Vehicles.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { format, differenceInDays } from "date-fns";
-import { nl } from "date-fns/locale";
+import { differenceInDays } from "date-fns";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
