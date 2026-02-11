20040eecdbf0f0351c787ff7394918b7e008552d
Ruben Van Der Velden <rubend3315@gmail.com>
2026-02-11 13:32:43 +0000

    File changes

--- functions/downloadContractPdf.ts
+++ functions/downloadContractPdf.ts
@@ -1,6 +1,5 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 import { jsPDF } from 'npm:jspdf@2.5.1';
-import { Jimp } from 'npm:jimp@1.6.0';
 
 function formatDate(dateStr) {
   if (!dateStr) return '';
