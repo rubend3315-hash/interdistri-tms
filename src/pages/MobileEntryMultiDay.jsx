07c5fb423715ab66f288b22c3a6ed3819a02a065
Ruben Van Der Velden <rubend3315@gmail.com>
2026-02-18 14:07:58 +0000

    File changes

--- src/pages/MobileEntryMultiDay.jsx
+++ src/pages/MobileEntryMultiDay.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo, useRef } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useOfflineSync } from "@/components/utils/useOfflineSync";
