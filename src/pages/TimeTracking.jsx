26b2410906f050a8f3972133c88482292bba11cc
Ruben Van Der Velden <rubend3315@gmail.com>
2026-02-20 16:23:00 +0000

    File changes

--- src/pages/TimeTracking.jsx
+++ src/pages/TimeTracking.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { createPageUrl } from "@/utils";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { getBreakMinutesForHours } from "../components/utils/breakScheduleUtils";
